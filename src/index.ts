c9a17c26ea38e6d5d52850ccf9d981bece3fa196
Peter Kieltyka <peter.kieltyka@gmail.com>
2023-12-05 16:45:28 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,15 @@
 // cloudflare-worker-relayer
 //
 
-// Specify window on global so we can ensure that `window.fetch` is available
-// @ts-ignore
-globalThis.window = globalThis
-
 import { ethers } from 'ethers'
-import { Session } from '@0xsequence/auth'
+import { Session, SessionSettings } from '@0xsequence/auth'
+import { networks, ChainId } from '@0xsequence/network'
+
 const contractAddress = '0x4574ca5b8b16d8e36d26c7e3dbeffe81f6f031f7'
 
 export interface Env {
 	PKEY: string;
 }
-
-// TODO: update example to include sequence relayer stuff... etc..
 
 // ethers provider -- here its important to use the static jcson rpc provider passing
 // the skipFetchSetup and also the chainId
@@ -24,6 +20,10 @@ const provider = new ethers.providers.StaticJsonRpcProvider({ url: nodeUrl, skip
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (env.PKEY === undefined || env.PKEY === '') {
+			console.log('ooops! check your wranger.toml config to set your PKEY')
+		}
+
 		const res = await call(request, env)
 		return new Response(`Hello World! ${res}`)
 	}
@@ -35,7 +35,23 @@ const call = async (request: Request, env: Env): Promise<any> => {
 
 	// Open a Sequence session, this will find or create
 	// a Sequence wallet controlled by your server EOA
+
+	// TODO: this is so ugly, we need to fix this in sequence.js
+	const settings: Partial<SessionSettings> = {
+		networks: [{
+			...networks[ChainId.POLYGON],
+			rpcUrl: nodeUrl,
+			relayer: {
+				url: 'https://polygon-relayer.sequence.app',
+				provider: {
+					url: nodeUrl
+				}
+			}
+		}],
+	}
+
 	const session = await Session.singleSigner({
+		settings: settings,
 		signer: walletEOA,
 	})
 	
@@ -54,10 +70,10 @@ const call = async (request: Request, env: Env): Promise<any> => {
 		data
 	}
 
-    try {
-        const res = await signer.sendTransaction(txn)
+	try {
+		const res = await signer.sendTransaction(txn)
 		return res
-	}catch(err){
+	} catch(err) {
 		return 'ERROR'
 	}
 }
